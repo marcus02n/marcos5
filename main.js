79cde03d8dcd3ca39ea11818494e289c185b19fb
marcus02n <133891132+marcus02n@users.noreply.github.com>
2023-09-18 12:26:58 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
 function tocaSomPom(){
+   document.querySelector('#som.tecla_pom').play();
+document.querySelector('.tecla_pom').onclick=tocaSomPom;}
 
-}
-document.querySelector('.tecla_pom');
+function tocaSomClap(){
+    document.querySelector('#som_tecla_clap').play();
+document.querySelector(',tecla_clap').onclick= tocaSomClap;}    
+
+document.querySelector('.tecla_tim');
+
+document.querySelector('.tecla_splash');
+
+document.querySelector('.tecla_toim');
+
+document.querySelector('.tecla_psh');
+
+document.querySelector(',tecla_tic');
+
+document.querySelector(',tecla_tom');
