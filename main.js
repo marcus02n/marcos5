e617a0e6ebb12e5f7d267fcf1f2ae4ad3c538834
marcus02n <133891132+marcus02n@users.noreply.github.com>
2023-10-25 12:20:59 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -4,13 +4,20 @@ function tocaSom(idElementoAudio){
 const listaDeTeclas = document.querySelectorA11('.tecla');
 
 //Estrutura de repetição - enquanto
-  for(let contador = 0;contador< listaDeTeclas.length;contador++){
+for(let contador = 0;contador< listaDeTeclas.length;contador++) {
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classlist[1];
     const idAudio = `#som_ ${instrumento}`;
-    
+    console.log(idAudio);
 
     tecla.onclick = function(){
       tocaSom(idAudio);
     };
+  tecla.onkeydown=function(){
+    tecla.classlist.add('ativa');
   }
+  tecla.onkeydown=function(){
+    tecla.classlist.remove('ativa');
+  }
+  console.log(contador);}
+
